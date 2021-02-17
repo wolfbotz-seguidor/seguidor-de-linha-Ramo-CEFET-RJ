811b0abd00790023913eb2d6c84a874bb8b13701
wolfbotz-seguidor <59606037+wolfbotz-seguidor@users.noreply.github.com>
2021-02-17 10:38:08 -0300

    Create PWM.h

---
+++ implementacao_futura/Códido base em PIC 18f4550/PWM.h
@@ -0,0 +1,7 @@
+//pwm1 -> motor A, da direita
+//pwm2 -> motor B, da esquerda
+
+//Protótipos:
+void PWM_Init ();
+void PWM1_Set_Duty (unsigned short pwm1);
+void PWM2_Set_Duty (unsigned short pwm2);
