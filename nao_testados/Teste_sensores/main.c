434d8b5feb3277ee99b1a7c04d10e37d21f338a7
Johann Steffanno Jerônimo da Costa Caniato Amorim <johannsamorim@gmail.com>
2021-06-28 16:37:16 -0300

    Update main.c

--- nao_testados/Teste_sensores/main.c
+++ nao_testados/Teste_sensores/main.c
@@ -12,7 +12,7 @@
 #include <util/delay.h>     //Biblioteca geradora de atraso
 #include "UART.h"           //Biblioteca da comunicação UART
 #include "ADC.h"            //Biblioteca do conversor AD
-#include "configbits.txt"   //configura os fusíveis
+//#include "configbits.txt"   //configura os fusíveis
 
 //variáveis de comando para os registradores
 #define set_bit(y,bit) (y|=(1<<bit)) //coloca em 1 o bit x da variável Y
@@ -21,8 +21,8 @@
 #define tst_bit(y,bit) (y&(1<<bit)) //retorna 0 ou 1 conforme leitura do bit
 
 
-#define sensor_de_curva PD2
-#define sensor_de_parada PD4
+//#define sensor_de_curva PD2
+//#define sensor_de_parada PD4
 
 #define sensor_tras_esquerdo PD6 
 #define sensor_tras_direito  PD7
@@ -52,28 +52,29 @@ ISR(USART_RX_vect) {
 //------------------------------------------------------
 
 void main(void) {
-    DDRD = 0b00000000; //PORTD como entrada
-    PORTD = 0b00000000; //PORTD inicia em 0
-
-
-    int sensores_traseiros [] = {sensor_tras_direito, sensor_tras_esquerdo};
-    int sensores_laterais [] = {sensor_de_curva, sensor_de_parada};
+    DDRD =  0b00000000;       //PORTD como entrada
+    PORTD = 0b10000000;       //PORTD inicia em 0, PD7 com pull up interno
+    DDRB =  0b00000000;       //PORTB como entrada
+    PORTB = 0b00111111;       //PB0 com pull up interno
+    DDRC =  0b00000000;       //PORTC como entrada
+    PORTC = 0b00001111;       //PORTC com pull up ativado
 
     UART_config(); //Inicializa a comunicação UART
     inicializa_ADC(); //Configura o ADC
     UART_enviaString(s); //Envia um texto para o computador
     
     while (1) {
+        int sensores_laterais [] = {(tst_bit(PINB, sensor_de_curva)) >> sensor_de_curva, tst_bit(PIND, sensor_de_parada) >> sensor_de_parada};
         int sensores_frontais[] = {le_ADC(0), le_ADC(1), le_ADC(2), le_ADC(3), le_ADC(4), le_ADC(6)};
-        int sensor_borda = le_ADC(5);
+        //int sensor_borda = le_ADC(5);
 
         //==========Teste dos sensores Frontais====//
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < 6; i++) {
             sprintf(buffer, "%4d", sensores_frontais[i]); //Converte para string
             UART_enviaString(buffer); //Envia para o computador
             UART_enviaCaractere(0x20); //espaço
         }
-        UART_enviaCaractere(0x0D); //pula linha
+        UART_enviaCaractere(0x0A); //pula linha
 
         //======Sensor de borda do Agostinho====//
         /*sprintf(buffer, "%4d", sensor_borda);
@@ -112,21 +113,21 @@ void main(void) {
         //==============Robô Van Grogue============// 
 
         //======Sensores frontais=====///
-        /*int sensores_frontais[] = {le_ADC(0), le_ADC(1), le_ADC(2), le_ADC(3), le_ADC(4), le_ADC(5)};
-        for (int i = 0; i < 7; i++) {
-            sprintf(buffer, "%4d\n", sensores_frontais[i]); //Converte para string
+        /*int sensores_frontais[] = {le_ADC(3), le_ADC(2), le_ADC(1), le_ADC(0), le_ADC(7), le_ADC(6)};
+        for (int i = 0; i < 6; i++) {
+            sprintf(buffer, "%4d", sensores_frontais[i]); //Converte para string
             UART_enviaString(buffer); //Envia para o computador
             UART_enviaCaractere(0x20); //espaço
         }
-        UART_enviaCaractere(0x0D); //pula linha
+        UART_enviaCaractere(0x0A); //pula linha*/
 
         //========Sensores laterais=====//
-        for (int i = 0; i < 2; i++) {
+        /*for (int i = 0; i < 2; i++) {
             sprintf(buffer, "%4d", sensores_laterais[i]); //Converte para string
             UART_enviaString(buffer); //Envia para o computador
             UART_enviaCaractere(0x20); //espaço
         }
-        UART_enviaCaractere(0x0D); //pula linha*/
+        UART_enviaCaractere(0x0A); //pula linha*/
 
     }
 }
