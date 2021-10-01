e5601c492be97963c69572643619287c49a82441
Johann Steffanno Jerônimo da Costa Caniato Amorim <johannsamorim@gmail.com>
2021-10-01 15:57:13 -0300

    Update ADC.h

--- nao_testados/Van_Grogue_AVR/ADC.h
+++ nao_testados/Van_Grogue_AVR/ADC.h
@@ -1,29 +1,39 @@
 /*---------------------------------------------------------------
  * BIBLIOTECA PARA UTILIZACAO DO CONVERSOR AD DO AVR
- * Modificada por: PROF. RODRIGO RECH
- * 08/2019
+ * Modificada por: PROF. André Costa Canella
+ * 08/2021
  * -----------------------------------------------------------------*/
 
-void inicializa_ADC(void){
+void ADC_init (void) {
+    //FADC = 1MHz
+    //Tadc = 1/1MHz = 1us
+    //Primeira Conversão = 25*1 = 25us
+    //Demais conversões = 14*1 = 14us
+    //Tempo total do prieiro ciclo = (25*1) + (14*1*5) = 95us
+    //Tempo das demais conversões = 14*1*6 = 84us
+    //Utilizando teoria de amostragem -> 168->190us
+
     
-    //Habilita a referência de tensão interna do ADC e ativa o canal 0 (VCC)
-    ADMUX = (1<<REFS0);                   
-    
-    //Habilita o ADC e configura o prescaler para 128
-    ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+    ADMUX = 0x60; //0110-0000   //Referência no AVCC
+    ADCSRA = 0x8c; //1000-1100  //ADC habilitado, interrupção do AD habilitado e prescaler de 16 - 1MHz
+    ADCSRB = 0x00; // 0000-0000 //Modo livre
+    DIDR0 = 0x3f;// 0011-1111   //Desabilita a entrada digital desses terminais
+ 
+    //Atenção. Segundo o datasheet quanto maior a velocidade,
+    //menor a precisão do AD, logo, utilizar 8bits em freq. elevadas
 }
 
-unsigned int le_ADC(unsigned char canal){
+void ADC_conv_ch (unsigned char canal) {
     
-    canal = canal & 0b00001111;
-    ADMUX = (ADMUX & 0xF0) | canal; 
-
-    //Inicia a conversão
-    ADCSRA |= (1 << ADSC);
-
-    //Aguarda a finalização da conversão
-    while ( (ADCSRA & (1<<ADSC)));
+    ADMUX &= 0xf0;
+    ADMUX |= (canal & 0x0f);
     
-    //Retorna o valor convertido
-    return ( ADC ); 
+    ADCSRA |= 0x40;
+            
+}
+
+unsigned char ADC_ler () {
+    
+    unsigned char dado = ADCH;
+    return dado;
 }
