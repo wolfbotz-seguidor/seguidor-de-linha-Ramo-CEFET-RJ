b5eeea59d8e42e0c088e130a260a7263c15f9f0c
Johann Steffanno Jerônimo da Costa Caniato Amorim <johannsamorim@gmail.com>
2021-12-13 17:24:50 -0300

    Update main.c

--- nao_testados/Van_Grogue_AVR/main.c
+++ nao_testados/Van_Grogue_AVR/main.c
@@ -19,6 +19,14 @@
 
 /*==============================================================*/
 
+/*Macros*/
+#define tempo           0
+#define distancia       1
+#define raio            2
+#define vel_linear      0       //vel_linear_média
+#define pwm_medio       1
+#define acel_medio      2
+
 /*Variáveis globais*/
 int erro = 0;      //variável para cáculo do erro da direção do robô em cima da linha
 unsigned int PWMA = 0, PWMB = 0; // Modulação de largura de pulso enviada pelo PID
@@ -37,7 +45,15 @@ char f_motor = 0;       //variável de controle da calibração automática
 char flag_curva = 0;    //cronometragem entre as retas e as cruvas
 char flag_parada = 0;   //inicia e encerra a cronometragem da pista
 char f_calibra = 0;     //vai pra 1 quando a calibração é finalizada
+float matriz_telemetria [100][3];   //matriz de colhimento de dados
+float matriz_pista      [30][3];    //colhe somente distancia, tempo e raio
+unsigned char f_stop = 0;           //encerra a rotina
+unsigned char f_millis = 0;         //controle do cronômetro
+float dist_right = 0, dist_left = 0;
+unsigned char f_dados_t = 0, f_dados_c = 0;         //t = telemetria, c = coleta
 
+/*Variáveis do encoder*/
+unsigned char pulse_numberR = 0, pulse_numberL = 0; //variáveis para contagem dos pulsos dos encoders
 
 /*Variáveis da UART*/
 volatile char ch; //armazena o caractere lido
@@ -66,12 +82,20 @@ void calibration();         //contêm toda a rotina de calibração
 void fim_de_pista();        //verifica se é o fim da psita
 void f_timers (void);       //função de temporização das rotinas
 void Auto_calibration(void);
+void telemetria(void);
+float velocid_linear();
+int valor_pwm();
+float speed_avrg();
 void volta_pra_pista(void);
-void millis(void);
+void envia_dados();
+unsigned int millis(void);
+float distancia_calculo(void);
+float calculo_do_raio(void);
 void f_timer1(void);
 void f_timer2(void);
 void f_timer3(void);
 void f_timer4(void);
+void f_timer5(void);
 /*===========================================================================*/
 
 /*Interrupções*/
@@ -594,45 +618,242 @@ void fim_de_pista()
     
     if(parada > 1)  //dois marcadores de parada
     {
-        //f_parada = 1;
-        //freio();
+        f_parada = 1;
+        freio();
         parada = 0;
     }
     
 }
 
-void millis(void)
+void coleta_de_dados(void)
 {
-    static unsigned int f_read = 0;
-    static unsigned int millis = 0;
+    static char flag_curva = 0;                 //quando passar por 
+                                               //um marcador de curva ou o último marcador de parada
+                                               //será guaradado o valor na matriz   
+    static char f_record = 0;
     
-    millis++;
+    static unsigned int i = 0;  //variável de posição da linha da matriz
     
     if(flag_parada)//criar uma função para printar na tela
     { 
-        if(flag_curva && !f_read)
+        if(flag_curva && !f_record)
         {
-            sprintf(buffer, "%dms\n", millis);
-            UART_enviaString(buffer);
-            millis = 0;
-            f_read = 1;
+            matriz_pista[i][tempo]      = millis();             //tempo em milissegundos
+            matriz_pista[i][distancia]  = distancia_calculo();  //distância em mm
+            matriz_pista[i][raio]       = calculo_do_raio();    //raio em mm 
+            //sprintf(buffer, "%dms\t", millis());
+            //UART_enviaString(buffer);
+            f_millis = 1;                  //zerando o timer
+            //sprintf(buffer, "%.2fmm\t", distancia_calculo());  //distância em milímetros
+            //UART_enviaString(buffer);
+            f_dados_c = 1;
+            envia_dados();
+            f_record = 1;                     //flag para gravar uma vez
         }
 
-        else if(!flag_curva && f_read)
+        else if(!flag_curva && f_record)
         {
-            f_read = 0;
-        }
-
-
+            f_record = 0;
+            f_millis = 0;               //voltando o timer
+        }       
+        
+       
         if(f_parada)
         {
-            sprintf(buffer, "%dms\n", millis);
-            UART_enviaString(buffer);
-            millis = 0;
+            matriz_pista[i][tempo]      = millis();
+            matriz_pista[i][distancia]  = distancia_calculo();
+            //sprintf(buffer, "%dms\t", millis());
+            //UART_enviaString(buffer);
+            f_millis = 1;                  //zerando o timer
+            //sprintf(buffer, "%.2fmm\t", distancia_calculo());  //distância em milímetros
+            //UART_enviaString(buffer);
             flag_parada = 0;
+            f_stop = 1;
         }
+        
+        i++;                //pulo a linha da matriz
+        
     }
 }
+
+float distancia_calculo(void)
+{
+    static float dist = 0;
+
+    dist_right = pulse_numberR * 0.812; //converte o número de pulsos em mm
+    dist_left = pulse_numberL * 0.812;
+    
+    pulse_numberR = 0x00;
+    pulse_numberL = 0x00; //zera as variáveis de cálculo do raio da curva
+
+    
+    dist = (dist_right + dist_left) / 2;    //em uma reta
+    
+    /*Criar condições para reta e curva e criar função de curva*/
+    return dist;
+}
+
+
+float calculo_do_raio() //esta função calcula o raio a partir da disância percorrida pelas duas rodas do robô
+{
+    static float raio_f = 0;            //raio
+    unsigned int diametro = 126;        //126mm, diâmetro sas rodas
+    
+    if((dist_right - dist_left) < 1 || (dist_right - dist_left) > -1)
+    {
+    
+        raio_f = (diametro / 2.0) * ((dist_right + dist_left) / (dist_right - dist_left)); //Cálculo do raio em módulo
+        
+        if(raio_f < 0)
+        {
+            raio_f *= -1;       //módulo
+        }
+    
+    }
+    
+    else
+    {
+        raio_f = 0;
+    }
+        
+        /*sprintf(buffer, "Raio %5d\n", raio_f); //Converte para string
+        UART_enviaString(buffer); //Envia para o computador
+        UART_enviaCaractere(0x0D); //pula linha*/
+ 
+
+    return raio_f;
+}
+
+unsigned int millis(void)
+{
+    //static unsigned int f_read = 0;
+    static unsigned int millis = 0;
+    
+    if (f_millis)
+    {
+        millis = 0;
+    }
+    else
+    {
+        millis++;
+    }
+    
+    return millis;
+}
+
+/*funções do encoder*/
+
+void count_pulsesD() {
+    static int Encoder_C1Last = 0, direction_m;
+
+    int Lstate = 0;//tst_bit(PIND, encoder_C1D); //variável de leitura de um dos pinos do encoderD
+
+    if (!Encoder_C1Last && Lstate) { //Verifica se Encoder_C1Last é falso e Lstate é verdadeiro
+        int val = 0;//tst_bit(PIND, encoder_C2D); //Variável de leitura do segundo pino do encoderD
+
+        if (!val && direction_m) direction_m = 0; //sentido horário
+
+        else if (val && !direction_m) direction_m = 1; //sentido anti-horário
+    }
+
+    Encoder_C1Last = Lstate;
+
+    if (!direction_m) pulse_numberR++; //sentido horário
+    else pulse_numberR--;
+
+
+
+}
+
+void count_pulsesE() {
+    static int Encoder_C1Last = 0, direction_m;
+
+    int Lstate = 0;//tst_bit(PIND, encoder_C1E); //variável de leitura de um dos pinos do encoderD
+
+    if (!Encoder_C1Last && Lstate) { //Verifica se Encoder_C1Last é falso e Lstate é verdadeiro
+        int val = 0;//tst_bit(PIND, encoder_C2E); //Variável de leitura do segundo pino do encoderD
+
+        if (!val && direction_m) direction_m = 0; //sentido horário
+
+        else if (val && !direction_m) direction_m = 1; //sentido anti-horário
+    }
+
+    Encoder_C1Last = Lstate;
+
+    if (!direction_m) pulse_numberR++; //sentido horário
+    else pulse_numberL--; //sentido anti-horário
+
+
+}
+
+void telemetria(void)
+{
+    static int i = 0;
+    
+    matriz_telemetria[i][vel_linear]    = velocid_linear();
+    matriz_telemetria[i][pwm_medio]     = valor_pwm();
+    matriz_telemetria[i][acel_medio]    = speed_avrg();
+    
+    f_dados_t = 1;
+    
+    envia_dados();
+}
+
+float velocid_linear()
+{
+    static float distancia_f = 0;
+    static float velocidade  = 0;
+    
+    distancia_f = distancia_calculo();
+    
+    velocidade  = distancia_f / 0.5;     //cálculo da velocidade em mm/s
+    
+    return velocidade;
+}
+
+
+int valor_pwm()
+{
+    return ((PWMA + PWMB) / 2);
+}
+
+float speed_avrg()
+{
+    return(velocid_linear() / 0.5);     //ca?culo da aceleração em mm/s²
+}
+
+void envia_dados()
+{
+    if(f_dados_t)
+    {
+        for(int linha = 0; linha < 100; linha++)
+        {
+            for(int coluna = 0; coluna < 3; coluna++)
+            {
+                sprintf(buffer, "%.2f\t", matriz_telemetria[linha][coluna]);
+                UART_enviaString(buffer);
+            }
+            UART_enviaCaractere('\n');
+        }  
+        f_dados_t = 0;
+    }
+    
+    if(f_dados_c)
+    {
+      for(int linha = 0; linha < 30; linha++)
+        {
+            for(int coluna = 0; coluna < 3; coluna++)
+            {
+                sprintf(buffer, "%.2f\t", matriz_pista[linha][coluna]);
+                UART_enviaString(buffer);
+            }
+            UART_enviaCaractere('\n');
+        }  
+        f_dados_c = 0;  
+    }
+}
+
+/*RTOS primitivo*/
 
 #define temp_min 1
 /*Parte não visível ao usuário*/
@@ -642,6 +863,7 @@ void f_timers (void)
     static unsigned char c_timer2 = 0;
     static unsigned char c_timer3 = 0;
     static unsigned char c_timer4 = 0;
+    static unsigned char c_timer5 = 0;
         
     //funções a cada 200us
     if(c_timer1 < 2 - temp_min)      //tempo que quer menos 1
@@ -690,6 +912,20 @@ void f_timers (void)
         f_timer4();
         c_timer4 = 0;
     }
+    
+    if(c_timer5 < 10 - temp_min)   //1000us
+    {
+        c_timer5++;
+    }
+
+    else
+    {
+        f_timer5();
+        c_timer5 = 0;
+    }
+    
+    
+    
 }//fim do programa
 
 void f_timer1(void)
@@ -703,26 +939,45 @@ void f_timer2(void)
     estrategia();
 }
 
-void f_timer3(void)
+void f_timer3(void)     //10ms
 {   
-    static unsigned char c_timer = 0;
+    static unsigned char c_timer1 = 0, c_timer2 = 0;
     
-    if(c_timer < 200 - temp_min)  //2000ms = 2s
+    if(c_timer1 < 200 - temp_min)  //2000ms = 2s
     {
-        c_timer++;
+        c_timer1++;
     }
 
     else 
     {   
         //Auto_calibration();
-        c_timer = 0;
+        c_timer1 = 0;
+    }
+    
+    if(c_timer2 < 50 - temp_min)  //500ms = 0,5s
+    {
+        c_timer2++;
+    }
+
+    else 
+    {   
+        telemetria();
+        c_timer2 = 0;
     }
 }
 
 void f_timer4(void)
 {
-    //millis();   //função chamada a cada 1ms 
+    millis();   //função chamada a cada 1ms 
 
+}
+
+void f_timer5(void)
+{
+    if(!f_stop)
+    {
+        coleta_de_dados();
+    }
 }
 
 
