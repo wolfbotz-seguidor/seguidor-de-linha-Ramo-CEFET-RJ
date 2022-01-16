d53b1f63cfe360633fab1c6653ecf2b56c6fa3fc
Jeff-Oliveira <JeffersonH.Feernandes@gmail.com>
2022-01-16 11:08:18 -0300

    Att - PWM.c && PWM.h
    Foram criadas duas novas funcoes:
    
    calc_duty : Converte o valor de entrada no formato de PWM para um valor equivalente em duty_cycle
    
    calc_PWM : Converte o valor de entrada no formato de duty_cycle para um valor equivalente em PWM

--- Robô millis/0.2_Testing/PWM.c
+++ Robô millis/0.2_Testing/PWM.c
@@ -74,15 +74,61 @@ void PWM_limit()
     if (PWMA < 0)
     {
       ExcessoB = (PWMA*(-1) * 2);
-      PWMA += (PWMA*(-1)*2);
-      PWMB += ExcessoB;
+         PWMA += (PWMA*(-1)*2);
+         PWMB += ExcessoB;
     }
 
     else if (PWMB < 0)
     {
       ExcessoA = (PWMB*(-1) * 2);
-      PWMB += (PWMB*(-1)*2);
-      PWMA += ExcessoA;
+         PWMB += (PWMB*(-1)*2);
+         PWMA += ExcessoA;
     }        
 
 }
+
+int calc_pwm(char duty_cycle)
+{
+    int pwm;
+    
+	// Limita o Duty Cycle mínimo e máximo
+         if(duty_cycle <   0) duty_cycle = 0;
+	else if(duty_cycle > 100) duty_cycle = 100;
+    
+    pwm = duty_cycle*10;
+			
+	return pwm;
+	
+} /* end calc_pwm */
+
+char calc_duty(int pwm)
+{	
+    char duty_cycle;
+    
+    // Limita o PWM mínimo e máximo
+         if(pwm <    0) pwm = 0;
+	else if(pwm > 1000) pwm = 1000;
+
+    duty_cycle = pwm/10;
+		
+    return duty_cycle;
+    
+} /* end calc_duty */
+
+/* ============================
+ * Tabela calc_pwm e calc_duty
+ * ......................
+ * |  Duty    |     PWM |
+ * ''''''''''''''''''''''
+ * |   10     |     100 |
+ * |   20     |     200 |
+ * |   30     |     300 |
+ * |   40     |     400 |
+ * |   50     |     500 |
+ * |   60     |     600 |
+ * |   70     |     700 |
+ * |   80     |     800 |
+ * |   90     |     900 |
+ * |  100     |    1000 |
+   ''''''''''''''''''''''
+===============================*/
--- Robô millis/0.2_Testing/PWM.h
+++ Robô millis/0.2_Testing/PWM.h
@@ -1,6 +1,10 @@
 #include <avr/io.h>
 
-void PWM_setDuty_2(int duty);
-void PWM_setDuty_1(int duty);
-void PWM_setFreq(char option) ;
+void PWM_setDuty_2(int  duty);
+void PWM_setDuty_1(int  duty);
+void PWM_setFreq  (char option);
+
 void PWM_limit();
+
+char calc_duty(int  pwm);
+int  calc_pwm (char duty_cycle);
